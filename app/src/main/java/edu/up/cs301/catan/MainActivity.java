4309b4a1d7457f330583dfa49adee1a197aebca2
goldey17 <goldey17@up.edu>
2015-09-17 09:37:41 -0700

    Popup is actually a popup now and works.

--- app/src/main/java/edu/up/cs301/catan/MainActivity.java
+++ app/src/main/java/edu/up/cs301/catan/MainActivity.java
@@ -3,10 +3,14 @@ package edu.up.cs301.catan;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 import static edu.up.cs301.catan.GameBoard.*;
 
@@ -23,12 +27,49 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void goToPlayerTradingMenu() {
-        Button tradeButton = (Button) findViewById(R.id.tradeButton);
+        final Button btnOpenPopup = (Button)findViewById(R.id.tradeButton);
+        btnOpenPopup.setOnClickListener(new Button.OnClickListener() {
 
-        tradeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,TradePopup.class));
+            public void onClick(View arg0) {
+                LayoutInflater layoutInflater
+                        = (LayoutInflater) getBaseContext()
+                        .getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = layoutInflater.inflate(R.layout.activity_trade_popup, null);
+                final PopupWindow popupWindow = new PopupWindow(
+                        popupView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+                Button btnDismiss = (Button) popupView.findViewById(R.id.goBack);
+                btnDismiss.setOnClickListener(new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                    }
+                });
+
+                Button tradePlayerButton = (Button) popupView.findViewById(R.id.tradeWithPlayers);
+                tradePlayerButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                        startActivity(new Intent(MainActivity.this, Trading.class));
+                    }
+                });
+
+                Button tradeBankButton = (Button) popupView.findViewById(R.id.tradeWithBank);
+                tradeBankButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        popupWindow.dismiss();
+                        startActivity(new Intent(MainActivity.this, Trading.class));
+                    }
+                });
+
+                popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
+
             }
         });
     }
