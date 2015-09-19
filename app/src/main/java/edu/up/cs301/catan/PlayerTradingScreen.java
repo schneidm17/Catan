a7f3a8e304dd13604e247f0f848ed670c2f1ed03
goldey17 <goldey17@up.edu>
2015-09-19 15:18:36 -0700

    Adding Player Trading Menu

---
+++ app/src/main/java/edu/up/cs301/catan/PlayerTradingScreen.java
@@ -0,0 +1,55 @@
+package edu.up.cs301.catan;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PlayerTradingScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_trading_screen);
+
+        goBackToGameBoard();
+    }//onCreate
+
+    //Function to exit the trading menu when any button is pushed
+    private void goBackToGameBoard() {
+        Button resumeGame = (Button) findViewById(R.id.returnToGame);
+        Button playerOneAccept = (Button) findViewById(R.id.player1_accept);
+        Button playerTwoAccept = (Button) findViewById(R.id.player2_accept);
+        Button playerThreeAccept = (Button) findViewById(R.id.player3_accept);
+
+        resumeGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        playerOneAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        playerTwoAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        playerThreeAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }//goBackToGameBoard
+
+}//PlayerTradingScreen
